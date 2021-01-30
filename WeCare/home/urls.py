012c85275bbc691405f6446ef2982cedfce8086e
niralishah01 <68777758+niralishah01@users.noreply.github.com>
2021-01-30 13:03:18 +0530

    NewsFeed is added

--- WeCare/home/urls.py
+++ WeCare/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index,doctorhome,adminhome
+from .views import index,doctorhome,adminhome,news
 
 urlpatterns=[
     path('',index),
     path('doctorhome/',doctorhome),
     path('adminhome/',adminhome),
+    path('news/',news),
 ]
--- WeCare/home/views.py
+++ WeCare/home/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponsePermanentRedirect
 from django.urls import reverse
+import requests
+# from newsapi import NewsApiClient
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-    #return HttpResponsePermanentRedirect(reverse('homepage'))
 
 def doctorhome(request):
     return render(request,'doctor_home.html')
 
 def adminhome(request):
     return render(request,'admin_home.html')
+
+def news(request):
+    # https://www.medicalnewstoday.com/
+    # newsapi = NewsApiClient(api_key='81606bad15824906a328e56705cc8f52')
+    # top_headlines = newsapi.get_top_headlines(q='bitcoin',
+    #                                       sources='bbc-news,the-verge',
+    #                                       category='business',
+    #                                       language='en',
+    #                                       country='us')
+    # print(top_headlines)
+    # return render(request,'home.html')
+    url = ('http://newsapi.org/v2/top-headlines?'
+        'q=medical&'
+        'apiKey=81606bad15824906a328e56705cc8f52')
+    response = requests.get(url)
+    # print(response.json())
+    results=response.json()['articles']
+    # print(results)
+    news=[]#title
+    image=[]#url for image
+    desc=[]#description
+    content=[]
+    urls=[]
+    for l in range(len(results)):
+        t=results[l]
+        news.append(t['title'])
+        image.append(t['urlToImage'])
+        desc.append(t['description'])
+        content.append(t['content'])
+        urls.append(t['url'])
+    mylist=zip(news,desc,content,image,urls)
+    return render(request,'news.html',{'mylist':mylist})
