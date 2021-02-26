517bb036d63b51dac46a8d064d9b2bb78c170f9f
krishnashah23 <68641771+krishnashah23@users.noreply.github.com>
2021-02-26 23:17:43 +0530

    Models updated

--- WeCare/registration/models.py
+++ WeCare/registration/models.py
@@ -12,3 +12,10 @@ class Disease(models.Model):
     Symptoms=models.CharField(max_length=300)
     Description=models.CharField(max_length=1000)
     Cause=models.CharField(max_length=500)
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=70, unique=True)
+    address = models.CharField(max_length=200)
+    location = models.CharField(max_length=50)
+    speciality = models.CharField(max_length=70)
+    timings = models.CharField(max_length=70)
