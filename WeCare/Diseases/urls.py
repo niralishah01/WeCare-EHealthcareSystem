8efdf5e7eb5d3f7645e4c22710d78cdd9a012ed6
krishnashah23 <68641771+krishnashah23@users.noreply.github.com>
2021-02-27 22:54:12 +0530

    views + urls updates

--- WeCare/Diseases/urls.py
+++ WeCare/Diseases/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import getdiseaseinfo,adddiseaseinfo,getsearchresults,viewdiseases,getupdateinfo,updatediseaseinfo
+from .views import getdiseaseinfo,adddiseaseinfo,viewskindiseases,getsearchresults,viewdiseases,getupdateinfo,updatediseaseinfo,homesearch,addskindiseaseinfo,getskindiseaseinfo
 from django.views.generic import TemplateView
 
 urlpatterns=[
     path('getdiseaseinfo/',getdiseaseinfo),
+    path('getskindiseaseinfo/',getskindiseaseinfo),
     path('adddiseaseinfo/',adddiseaseinfo),
     path('',TemplateView.as_view(template_name='searchresults.html'),name="searchpage"),
     path('getsearchresults/',getsearchresults),
@@ -11,4 +12,7 @@ urlpatterns=[
     path('getupdateinfo/',getupdateinfo),
     path('updatediseaseinfo/',updatediseaseinfo),
     #path('searchdiseases/',searchdiseases),
+    path('homesearch/',homesearch),
+    path('addskindiseaseinfo/',addskindiseaseinfo),
+    path('viewskindiseases/',viewskindiseases),
 ]
--- WeCare/Diseases/views.py
+++ WeCare/Diseases/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.template.context_processors import csrf
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.db.models import Q
-from registration.models import Disease
+from registration.models import Disease,SkinDisease
 
 # # Create your views here.
 def getdiseaseinfo(request):
@@ -30,14 +30,14 @@ def adddiseaseinfo(request):
         return render(request,'adddiseaseinfo.html',{'msg1':'Inforation for this Disease is already added, you can update it later....'})
 
 def getsearchresults(request):
-    query=request.GET['q']
+    query=request.GET.get('q','')
     submitbutton= request.GET['search']
     print(query)
     if query!='':
         lookups=Q(name__icontains=query)
-        #print(lookups)
+        print(lookups)
         object_list=Disease.objects.filter(lookups)
-        #print(object_list)
+        print(object_list)
         if (object_list):
             return render(request,'searchresults.html',{'objectlist':object_list,'found':True})
         else:
@@ -45,13 +45,8 @@ def getsearchresults(request):
     else:
         return render(request,'searchresults.html',{'errmsg':"Enter appropriate search value"})
 def viewdiseases(request):
-    f=request.GET['found']
-    print(f)
     diseases=Disease.objects.all()
-    if(f==True):
-        return render(request,'viewdiseases.html',{'diseases':diseases,'found':f})
-    else:
-         return render(request,'viewdiseases.html',{'diseases':diseases,'notfound':f})
+    return render(request,'viewdiseases.html',{'diseases':diseases})
 
 def getupdateinfo(request):
     dname=request.GET['dname']
@@ -68,9 +63,53 @@ def updatediseaseinfo(request):
     desc=request.POST['description']
 
     Disease.objects.filter(name=disease_name).update(Symptoms=disease_symptoms,Cause=disease_cause,Description=desc)
-    return HttpResponseRedirect('/Diseases/viewdiseases/?found=True')
+    return HttpResponseRedirect('/Diseases/viewdiseases')
 
 def deletedisease(request):
     disease_name=request.GET['dname']
     d= Disease.objects.get(name=disease_name)
     d.delete()
+    return HttpResponseRedirect('/Diseases/viewdieseases')
+
+def homesearch(request):
+    query=request.GET.get('q','')
+    print(query)
+    if query!='':
+        lookups=Q(name__icontains=query)
+        print(lookups)
+        object_list=Disease.objects.filter(lookups)
+        print(object_list)
+        if (object_list):
+            return render(request,'homesearchresult.html',{'objectlist':object_list,'found':True})
+        else:
+            return render(request,'homesearchresult.html',{'errmsg':"SORRY: No search result found...."})
+    else:
+        return render(request,'homesearchresult.html',{'errmsg':"Enter appropriate search value"})
+
+def getskindiseaseinfo(request):
+    c={}
+    c.update(csrf(request))
+    return render(request,'addskindisease.html',c)
+
+def addskindiseaseinfo(request):
+    disease_name=request.POST.get('disease_name','')
+    image1 = request.FILES.get('image1','')
+    image2 = request.FILES.get('image2','')
+    image3 = request.FILES.get('image3','')
+    count=0
+    diseases=SkinDisease.objects.all()
+    if diseases is not None:
+        for dis in diseases:
+            if dis.name==disease_name:
+                count=1
+                break
+    if(count!=1):
+        d=SkinDisease(name=disease_name,image1=image1,image2=image2,image3=image3)
+        d.save()
+        return HttpResponseRedirect('/home/doctorhome')
+    else:
+        return render(request,'addskindisease.html',{'msg1':'Information for this Disease is already added, you can update it later....'})
+
+def viewskindiseases(request):
+    diseases=SkinDisease.objects.all()
+    return render(request,'viewskindiseases.html',{'diseases':diseases})
