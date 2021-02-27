58622b6393a6bcd439c3739cc6e16ecacd8fb3b4
krishnashah23 <68641771+krishnashah23@users.noreply.github.com>
2021-02-27 22:36:48 +0530

    Settings.py file updated

--- WeCare/WeCare/settings.py
+++ WeCare/WeCare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g5!5+paf%w#2*mhl*$x)x_92mn-*q@)r7k+$5mjke&e63_dgqz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'registration',
     'covid19Data',
     'Diseases',
-    'hospital'
+    'hospital',
 ]
 
 MIDDLEWARE = [
@@ -125,10 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST='smtp.gmail.com'
-# EMAIL_HOST_USER='niralipshah2000@gmail.com'
-# EMAIL_HOST_PASSWORD='eqdvzcmvwfptdoaa'
-# EMAIL_HOST_PORT=587
-# EMAIL_USE_TLS=True
-# DEFAULT_FROM_EMAIL='WeCare <noreply@wecare.com>'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'Diseases') 
+MEDIA_URL = '/Diseases/'
