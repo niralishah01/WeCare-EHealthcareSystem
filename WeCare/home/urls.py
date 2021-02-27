e09c080eb96c87ab8f83e9888765a1fff8149aff
krishnashah23 <68641771+krishnashah23@users.noreply.github.com>
2021-02-27 22:38:12 +0530

    Front-end modified

--- WeCare/home/urls.py
+++ WeCare/home/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import index,doctorhome,adminhome,news,getprofile,updateprofile,gotosearch,search,hospitalsearch
+from .views import index,doctorhome,adminhome,hospitalsearch,news,getprofile,updateprofile,gotosearch,search
 
 urlpatterns=[
     path('',index),
     path('doctorhome/',doctorhome),
+    path('hospitalsearch/',hospitalsearch),
     path('adminhome/',adminhome),
     path('news/',news),
     path('getprofile/',getprofile),
     path('updateprofile/',updateprofile),
     path('gotosearch/',gotosearch),
     path('search/',search),
-    path('hospitalsearch/',hospitalsearch),
 ]
