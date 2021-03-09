c5d020876bd0d2f59628824db21b56a56161a67f
68777758+niralishah01@users.noreply.github.com <68777758+niralishah01@users.noreply.github.com>
2021-03-09 14:24:39 +0530

    Diseasetreatment module is added+ skindiseases images are added

--- WeCare/Diseases/urls.py
+++ WeCare/Diseases/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import getdiseaseinfo,adddiseaseinfo,viewskindiseases,getsearchresults,viewdiseases,getupdateinfo,updatediseaseinfo,homesearch,addskindiseaseinfo,getskindiseaseinfo
+from .views import getdiseaseinfo,adddiseaseinfo,getsearchresults,viewdiseases,getupdateinfo,updatediseaseinfo,homesearch,deletedisease,getsearchresults2
+from .views import getskindiseaseinfo,addskindiseaseinfo,viewskindiseases,searchskindiseasefromimage,getdiseasetreatmentinfo,adddiseasetreatment,gettreatmentdetails,updatediseasetreatment
 from django.views.generic import TemplateView
 
 urlpatterns=[
@@ -7,12 +8,21 @@ urlpatterns=[
     path('getskindiseaseinfo/',getskindiseaseinfo),
     path('adddiseaseinfo/',adddiseaseinfo),
     path('',TemplateView.as_view(template_name='searchresults.html'),name="searchpage"),
+    path('',TemplateView.as_view(template_name='searchresults2.html'),name="treatmentsearchpage"),
     path('getsearchresults/',getsearchresults),
+    path('getsearchresults2/',getsearchresults2),
     path('viewdiseases/',viewdiseases),
     path('getupdateinfo/',getupdateinfo),
     path('updatediseaseinfo/',updatediseaseinfo),
+    path('deletedisease/',deletedisease),
     #path('searchdiseases/',searchdiseases),
     path('homesearch/',homesearch),
     path('addskindiseaseinfo/',addskindiseaseinfo),
     path('viewskindiseases/',viewskindiseases),
+    path('searchskindiseasefromimage/',searchskindiseasefromimage),
+    path('getdiseasetreatmentinfo/',getdiseasetreatmentinfo),
+    path('adddiseasetreatment/',adddiseasetreatment),
+    path('gettreatmentdetails/',gettreatmentdetails),
+    path('updatediseasetreatment/',updatediseasetreatment),
+    path('',TemplateView.as_view(template_name='searchresults2.html'),name="treatmentsearchpage"),
 ]
--- WeCare/Diseases/views.py
+++ WeCare/Diseases/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.template.context_processors import csrf
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.db.models import Q
-from registration.models import Disease,SkinDisease
+from registration.models import Disease,SkinDisease,DiseaseTreatment
 
 # # Create your views here.
 def getdiseaseinfo(request):
@@ -27,23 +27,23 @@ def adddiseaseinfo(request):
         d.save()
         return HttpResponseRedirect('/home/doctorhome')
     else:
-        return render(request,'adddiseaseinfo.html',{'msg1':'Inforation for this Disease is already added, you can update it later....'})
+        return render(request,'adddiseaseinfo.html',{'msg1':'Information for this Disease is already added, you can update it later....'})
 
 def getsearchresults(request):
-    query=request.GET.get('q','')
+    query=request.GET['q']
     submitbutton= request.GET['search']
-    print(query)
     if query!='':
         lookups=Q(name__icontains=query)
-        print(lookups)
+        #print(lookups)
         object_list=Disease.objects.filter(lookups)
-        print(object_list)
+        #print(object_list)
         if (object_list):
             return render(request,'searchresults.html',{'objectlist':object_list,'found':True})
         else:
             return render(request,'searchresults.html',{'errmsg':"SORRY: No search result found...."})
     else:
         return render(request,'searchresults.html',{'errmsg':"Enter appropriate search value"})
+
 def viewdiseases(request):
     diseases=Disease.objects.all()
     return render(request,'viewdiseases.html',{'diseases':diseases})
@@ -63,13 +63,13 @@ def updatediseaseinfo(request):
     desc=request.POST['description']
 
     Disease.objects.filter(name=disease_name).update(Symptoms=disease_symptoms,Cause=disease_cause,Description=desc)
-    return HttpResponseRedirect('/Diseases/viewdiseases')
+    return HttpResponseRedirect('/Diseases/viewdiseases/')
 
 def deletedisease(request):
     disease_name=request.GET['dname']
     d= Disease.objects.get(name=disease_name)
     d.delete()
-    return HttpResponseRedirect('/Diseases/viewdieseases')
+    return HttpResponseRedirect('/Diseases/viewdiseases/')
 
 def homesearch(request):
     query=request.GET.get('q','')
@@ -113,3 +113,69 @@ def addskindiseaseinfo(request):
 def viewskindiseases(request):
     diseases=SkinDisease.objects.all()
     return render(request,'viewskindiseases.html',{'diseases':diseases})
+
+def searchskindiseasefromimage(request):
+    data=SkinDisease.objects.all()
+    return render(request,'searchskindiseasefromimage.html',{'diseases':data})
+
+def getdiseasetreatmentinfo(request):
+    diseasedata=Disease.objects.all()
+    c={}
+    c.update(csrf(request))
+    return render(request,'adddiseasetreatment.html',{'c':c,'diseasedata':diseasedata})
+
+def adddiseasetreatment(request):
+    desc=request.POST['desc']
+    dur=request.POST['dur']
+    dis=request.POST['disease']
+    d=Disease.objects.get(name=dis)
+    dts=DiseaseTreatment.objects.all()
+    found=False
+    for dt in dts:
+        if(dt.Disease_id==d):
+            found=True
+    if(found):
+        diseasedata=Disease.objects.all()
+        c={}
+        c.update(csrf(request))
+        return render(request,'adddiseasetreatment.html',{'c':c,'diaseasedata':diseasedata,'msg1':'Treatment is already added for ths disease.. you can add more etails in it later.'})
+    dt=DiseaseTreatment(description=desc,duration=dur,Disease_id=d)
+    dt.save()
+    return HttpResponseRedirect('/home/doctorhome/')
+
+def gettreatmentdetails(request):
+    disnm=request.GET['dname']
+    dis=Disease.objects.get(name=disnm)
+    dst=DiseaseTreatment.objects.filter(Disease_id=dis)
+    if(not dst):
+        return render(request,'viewupdatediseasetreatmentdetails.html',{'msg':'Treatment for this disease is not available yet,it will be available soon..You cam also ask about this in our FAQ section'})
+    dt=DiseaseTreatment.objects.get(Disease_id=dis)
+    print(dt.Disease_id.name)
+    c={}
+    c.update(csrf(request))
+    return render(request,'viewupdatediseasetreatmentdetails.html',{'c':c,'dt':dt})
+
+def updatediseasetreatment(request):
+    dname=request.POST['dname']
+    desc=request.POST['desc']
+    dur=request.POST['dur']
+    d=Disease.objects.get(name=dname)
+    dt=DiseaseTreatment.objects.get(Disease_id=d)
+    dt.description=desc
+    dt.duration=dur
+    dt.save()
+    return HttpResponseRedirect('/Diseases/viewdiseases/')
+
+def getsearchresults2(request):
+    query=request.GET['q']
+    submitbutton= request.GET['search']
+    if query!='':
+        lookups=Q(name__icontains=query)
+        #print(lookups)
+        object_list=DiseaseTreatment.objects.filter(lookups)
+        if (object_list):
+            return render(request,'searchresults2.html',{'objectlist':object_list,'found':True})
+        else:
+            return render(request,'searchresults2.html',{'errmsg':"SORRY: No search result found...."})
+    else:
+        return render(request,'searchresults2.html',{'errmsg':"Enter appropriate search value"})
